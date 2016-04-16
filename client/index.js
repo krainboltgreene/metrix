fde9d103b79f723b19d7cd1603d1f608e63a1e28
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-04-16 01:46:52 -0700

    Switching over to cycles

--- client/index.js
+++ client/index.js
@@ -1,3 +1,6 @@
+import {Observable} from "rx"
+import {run} from "@cycle/core"
+import {makeHTTPDriver} from "@cycle/http"
 import {init} from "snabbdom"
 import snabClass from "snabbdom/modules/class"
 import snabProps from "snabbdom/modules/props"
@@ -10,13 +13,43 @@ const patch = init([
   snabProps,
   snabStyle
 ])
-const application = document.getElementById("application")
-const state = {
-  memory: {
-    free: 1241837568,
-    total: 17179869184,
-    updatedAt: new Date()
+const application = () => document.getElementById("application")
+
+const MINIMUM_TIMEOUT = 750
+const requestMemory$ = (tempo$) => tempo$.map(() => ({
+  url: "http://localhost:8081/memories",
+  method: "GET",
+  query: {
+    order: "created_at.desc",
+    select: "data,created_at"
+  },
+  headers: {
+    "Accept": "application/json",
+    "Range-Unit": "items",
+    "Range": "0-0"
+  }
+}))
+const receiveMemory$ = (http$) => http$.filter((response) => response.request.url === "http://localhost:8081/memories").switch().map((response) => response.body[0])
+
+const main = ({http}) => {
+  const tempo$ = Observable.interval(MINIMUM_TIMEOUT)
+  const outgoing$ = Observable.merge(
+    requestMemory$(tempo$)
+  ).share()
+  const incoming$ = Observable.combineLatest(
+    receiveMemory$(http),
+    (memory) => ({memory})
+  ).share()
+
+  incoming$.forEach((state) => patch(application(), layout(state)))
+
+  return {
+    http: outgoing$
   }
 }
 
-patch(application, layout(state))
+const drivers = {
+  http: makeHTTPDriver()
+}
+
+run(main, drivers)
