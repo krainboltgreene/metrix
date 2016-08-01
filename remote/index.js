29dda44f071c5c85865fbe81318a61e6fb803a84
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-07-31 22:43:51 -0700

    Clearing up the roles of each iteration

--- remote/index.js
+++ remote/index.js
@@ -1,4 +1,5 @@
 import store from "./store"
 
+export {
   store
 }
--- worker/index.js
+++ worker/index.js
@@ -1,7 +1,11 @@
 import memory from "./memory"
+import designers from "./designers"
 import scrub from "./scrub"
 
 const SECOND = 1000
+const FREQUENT = 1
+const MODERATE = 5
+const LAZY = 10
 
-setInterval(memory, 1 * SECOND)
-setInterval(scrub, 30 * SECOND)
+setInterval(memory, FREQUENT * SECOND)
+setInterval(scrub, LAZY * SECOND)
