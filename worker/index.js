62b12fd68a488440614164edafec6de884c6985f
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-08-03 00:00:07 -0700

    Lazy actually needs to be lazy

--- worker/index.js
+++ worker/index.js
@@ -5,7 +5,7 @@ import scrub from "./scrub"
 const SECOND = 1000
 const FREQUENT = 1
 const MODERATE = 5
-const LAZY = 10
+const LAZY = 15
 
 setInterval(memory, FREQUENT * SECOND)
 setInterval(scrub, LAZY * SECOND)
