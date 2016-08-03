2a437bdf8dcfe90b87514c380f933ab8e9dc4e46
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-08-02 23:59:29 -0700

    A few more smart formats

--- client/formatting/index.js
+++ client/formatting/index.js
@@ -2,9 +2,11 @@ import humanFormat from "human-format"
 import prettySize from "prettysize"
 
 const percent = (value) => `${value * 100}%`
+const money = (value) => `$${value / 100}`
 
 export {
   humanFormat,
   prettySize,
-  percent
+  percent,
+  money
 }
