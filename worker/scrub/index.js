82847c382fd9cc652584003522050fd315b21774
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-08-03 00:00:58 -0700

    Be smarter about promises

--- worker/scrub/index.js
+++ worker/scrub/index.js
@@ -4,11 +4,9 @@ import {store} from "~/remote"
 const LIMIT = 5
 
 export default function scrub () {
-  return Promise.all([
-    store("statistics")
-      .whereNotBetween("created_at", [new Date(ago(LIMIT, "minute")), new Date()])
-      .delete()
-  ])
-  .then(() => console.log("Deleted extra records"))
-  .catch(console.error)
+  return store("statistics")
+    .whereNotBetween("created_at", [new Date(ago(LIMIT, "minute")), new Date()])
+    .delete()
+    .then(() => console.log("Deleted extra records"))
+    .catch(console.error)
 }
