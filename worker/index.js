f934bfed92bb6fa86b48403ea7cd319b9f0caad2
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-08-03 00:00:24 -0700

    Shorter heartbeats means finer grained data

--- worker/index.js
+++ worker/index.js
@@ -2,7 +2,7 @@ import memory from "./memory"
 import designers from "./designers"
 import scrub from "./scrub"
 
-const SECOND = 1000
+const SECOND = 500
 const FREQUENT = 1
 const MODERATE = 5
 const LAZY = 15
