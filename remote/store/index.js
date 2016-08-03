dbcebb2fa48d7de7504583742aa03c42f39dc3a9
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-08-03 00:43:22 -0700

    Setting up a sharable client

--- remote/store/index.js
+++ remote/store/index.js
@@ -1,5 +1,6 @@
 import Dotenv from "dotenv"
 import requireEnvironmentVariables from "require-environment-variables"
+import {createClient} from "then-redis"
 
 Dotenv.load({silent: true})
 
@@ -7,4 +8,4 @@ requireEnvironmentVariables([
   "NODE_ENV"
 ])
 
-export default null
+export default createClient()
