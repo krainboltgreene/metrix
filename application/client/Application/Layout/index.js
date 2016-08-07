e59c4a7019e79432ca8a5be4ecb09244a4cd8ca7
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-08-07 16:41:42 -0700

    This animation looks a lot better

--- application/client/Application/Layout/index.js
+++ application/client/Application/Layout/index.js
@@ -2,7 +2,7 @@ import React, {PropTypes} from "react"
 
 export default function Layout ({children}) {
   return <section
-    className="animated pulse"
+    className="animated bounceIn"
     style={{
       display: "flex",
       flexDirection: "row",
