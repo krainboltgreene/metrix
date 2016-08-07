5d50e55f088b2b1c615f38fed5103027f7aed25d
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-08-07 16:41:18 -0700

    Consolidating the formatting options

--- application/client/Application/FreeMemory/index.js
+++ application/client/Application/FreeMemory/index.js
@@ -1,13 +1,14 @@
 import React from "react"
 import {pipe} from "ramda"
-import {prettySize} from "~/application/client/formatting"
+import {prettySize} from "~/application/library"
+import {toNumber} from "~/application/library"
 import Number from "../Number"
 
 export default function FreeMemory () {
   return <Number
     title="Free Memory"
     storeType="freeMemory"
-    format={pipe((value) => parseInt(value, 10), prettySize)}
+    format={pipe(toNumber, prettySize)}
     size="large"
   />
 }
--- application/library/index.js
+++ application/library/index.js
@@ -1,2 +1,5 @@
 export {default as hammer} from "./hammer"
 export {default as mapKeys} from "./mapKeys"
+export {default as humanFormat} from "human-format"
+export {default as prettySize} from "prettysize"
+export const toNumber = (value) => parseInt(value, 10)
