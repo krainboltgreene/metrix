d85b92510d50a9025138b4050d7fc4c23f5d2f11
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-08-07 16:42:17 -0700

    Instead of passing the streams, pass the parts we care about

--- application/client/Application/Number/index.js
+++ application/client/Application/Number/index.js
@@ -1,31 +1,39 @@
 import React, {Component, PropTypes} from "react"
 import {connect} from "react-redux"
-import {path} from "ramda"
+import {pathOr} from "ramda"
 import {merge} from "ramda"
 import {isNil} from "ramda"
 import {equals} from "ramda"
 import {none} from "ramda"
+import {zipObj} from "ramda"
 import BoxBody from "../BoxBody"
 import BoxHeader from "../BoxHeader"
 import BoxValue from "../BoxValue"
 import BoxTime from "../BoxTime"
 import Loading from "../Loading"
 
-const connectWithStreams = connect(
-  merge
+const connectLatest = connect(
+  (state, props) => {
+    return merge(
+      props,
+      zipObj(["timestamp", "value"], pathOr([], ["streams", props.storeType, "latest"], state))
+    )
+  }
 )
 
-export default connectWithStreams(class Number extends Component {
+export default connectLatest(class Number extends Component {
   static propTypes = {
     title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string,
     storeType: PropTypes.string.isRequired,
     format: PropTypes.func.isRequired,
     size: PropTypes.string,
-    streams: PropTypes.instanceOf(Object)
+    timestamp: PropTypes.string,
+    value: PropTypes.string
   }
 
   shouldComponentUpdate (props) {
-    return !equals(this.props, props)
+    return !equals([this.props.timestamp, this.props.value], [props.timestamp, props.value])
   }
 
   maybeRender (properties, components) {
@@ -41,8 +49,8 @@ export default connectWithStreams(class Number extends Component {
     const {storeType} = this.props
     const {format} = this.props
     const {size} = this.props
-    const {streams} = this.props
-    const [timestamp, value] = path([storeType, "latest"], streams) || []
+    const {timestamp} = this.props
+    const {value} = this.props
 
     return <BoxBody>
       <BoxHeader>{title}</BoxHeader>
