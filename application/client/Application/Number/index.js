0c36ce755fe02da1b2d91440897232e48c667892
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-08-07 21:21:59 -0700

    Fuck timestamps for now

--- application/client/Application/Number/index.js
+++ application/client/Application/Number/index.js
@@ -15,7 +15,6 @@ import BoxBody from "../BoxBody"
 import BoxHeader from "../BoxHeader"
 import BoxSubtitle from "../BoxSubtitle"
 import BoxValue from "../BoxValue"
-import BoxTime from "../BoxTime"
 import Loading from "../Loading"
 
 const connectToTimeseries = connect(
@@ -59,7 +58,6 @@ export default connectToTimeseries(class Number extends Component {
     const {subtitle} = this.props
     const {format} = this.props
     const {size} = this.props
-    const {timestamp} = this.props
     const {timeseries} = this.props
     const {value} = this.props
 
@@ -67,7 +65,6 @@ export default connectToTimeseries(class Number extends Component {
       <BoxHeader>{title}</BoxHeader>
       {this.maybeRender([subtitle], null, () => <BoxSubtitle>{subtitle}</BoxSubtitle>)}
       {this.maybeRender([value, format], <Loading />, () => <BoxValue size={size}>{format(value)}</BoxValue>)}
-      <BoxTime timestamp={new Date(timestamp)} />
       {
         this.maybeRender(
           [timeseries],
