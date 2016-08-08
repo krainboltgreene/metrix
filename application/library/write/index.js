b44eaaf1eb72fb41f01fc51353e1442e146768d5
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-08-08 09:01:56 -0700

    We're passing milliseconds, so lets use the millisecond expire

--- application/library/write/index.js
+++ application/library/write/index.js
@@ -3,5 +3,5 @@ import {store} from "~/application/remote"
 export default function write ({type, key, value, expire}) {
   return store.set(key, value)
     .then(() => store.sadd("types", type))
-    .then(() => store.expire(key, expire))
+    .then(() => store.pexpire(key, expire))
 }
