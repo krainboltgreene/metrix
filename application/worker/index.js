ff22e74d48dbed324a32faa163903f0644a74a08
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-08-07 21:13:19 -0700

    We're going to be expiring per key

--- application/worker/index.js
+++ application/worker/index.js
@@ -1,7 +1,6 @@
 import totalMemory from "./totalMemory"
 import freeMemory from "./freeMemory"
 import usedMemory from "./usedMemory"
-import scrub from "./scrub"
 
 const SECOND = 1000
 const FREQUENT = 1
@@ -10,4 +9,3 @@ const LAZY = 15
 setInterval(totalMemory, LAZY * SECOND)
 setInterval(freeMemory, FREQUENT * SECOND)
 setInterval(usedMemory, FREQUENT * SECOND)
-setInterval(scrub, FREQUENT * SECOND)
