ce9661ab476b83ac9384939703bbdd3fe119a32f
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-08-16 20:15:51 -0700

    Instead of something fancy, lets go simple for now

--- application/client/Application/Loading/index.js
+++ application/client/Application/Loading/index.js
@@ -1,9 +1,7 @@
 import React from "react"
-import Spinner from "react-spinkit"
 
 export default function Loading () {
-  return <Spinner
-    spinnerName="double-bounce"
-    noFadeIn
-  />
+  return <p>
+    <em>No data available yet...</em>
+  </p>
 }
