2e1a8e489112dd54b2fc315dc936a7924bcca5be
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-09-23 09:24:32 -0700

    I'm moving this to ramada-extra

--- application/library/mapKeys/index.js
+++
@@ -1,9 +0,0 @@
-import {curry} from "ramda"
-import {fromPairs} from "ramda"
-import {map} from "ramda"
-import {adjust} from "ramda"
-import {toPairs} from "ramda"
-
-const FIRST = 0
-
-export default curry((ƒunction, record) => fromPairs(map(adjust(ƒunction, FIRST), toPairs(record))))
