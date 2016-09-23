2f536ad8a61a5496104a4968a03acbf7d9201128
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-09-23 09:25:40 -0700

    Start logging the expire

--- application/worker/write/index.js
+++ application/worker/write/index.js
@@ -1,11 +1,12 @@
-import {store} from "~/application/remote"
+import {store} from "../remote"
 
 export default function write ({type, value, expire}) {
   const updatedAt = Date.now()
   const key = `${type}/${updatedAt}`
 
-  return store.set(key, value)
+  return store
+    .set(key, value)
     .then(() => store.sadd("types", type))
-    .then(() => store.pexpire(key, expire * 5))
-    .then(() => console.log({type, key, value}))
+    .then(() => store.pexpire(key, expire))
+    .then(() => console.log({type, key, value, expire}))
 }
