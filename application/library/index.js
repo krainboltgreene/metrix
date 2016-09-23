4eb719900fafeeb1c33114062d7b83e3fe0e960a
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-09-23 09:25:28 -0700

    Much better location

--- application/library/index.js
+++
@@ -1,8 +0,0 @@
-export {default as hammer} from "./hammer"
-export {default as mapKeys} from "./mapKeys"
-export {default as write} from "./write"
-export const SECOND = 1000
-export const MINUTE = SECOND * 60
-export const HOUR = MINUTE * 60
-export const DAY = HOUR * 24
-export const WEEK = DAY * 7
