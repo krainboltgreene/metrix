a97be6bacf43908effdcf2c77e5cb276f4b81be1
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-09-23 09:32:10 -0700

    Whoops

--- application/worker/index.js
+++ application/worker/index.js
@@ -2,6 +2,7 @@ import totalMemory from "./totalMemory"
 import freeMemory from "./freeMemory"
 import usedMemory from "./usedMemory"
 
+const initiate = (ƒunction) => {
   ƒunction()[0]()
   ƒunction()[0]()
   setInterval(...ƒunction())
